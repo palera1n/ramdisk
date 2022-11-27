5f31488286d771920efa1fd237bfc596c1bb96d8
Tom E <guacaplushy@icloud.com>
2022-11-27 12:39:18 -0500

    revert pyimg4-ifying

--- sshrd.sh
+++ sshrd.sh
@@ -147,26 +147,23 @@ cd ..
 "$dir"/gaster decrypt work/"$(awk "/""${replace}""/{x=1}x&&/iBSS[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//')" work/iBSS.dec
 "$dir"/gaster decrypt work/"$(awk "/""${replace}""/{x=1}x&&/iBEC[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//')" work/iBEC.dec
 "$dir"/iBoot64Patcher work/iBSS.dec work/iBSS.patched
-python3 -m pyimg4 im4p create -i work/iBSS.patched -o work/iBSS.patched.im4p -f ibss
-python3 -m pyimg4 img4 create -p work/iBSS.patched.im4p -m work/IM4M -o sshramdisk/iBSS.img4
+"$dir"/img4 -i work/iBSS.patched -o sshramdisk/iBSS.img4 -M work/IM4M -A -T ibss
 "$dir"/iBoot64Patcher work/iBEC.dec work/iBEC.patched -b "rd=md0 debug=0x2014e wdt=-1 `if [ -z "$2" ]; then :; else echo "$2=$3"; fi` `if [ "$check" = '0x8960' ] || [ "$check" = '0x7000' ] || [ "$check" = '0x7001' ]; then echo "-restore"; fi`" -n
-python3 -m pyimg4 im4p create -i work/iBEC.patched -o work/iBEC.patched.im4p -f ibec
-python3 -m pyimg4 img4 create -p work/iBEC.patched.im4p -m work/IM4M -o sshramdisk/iBEC.img4
+"$dir"/img4 -i work/iBEC.patched -o sshramdisk/iBEC.img4 -M work/IM4M -A -T ibec
 
-python3 -m pyimg4 im4p extract -i work/"$(awk "/""${replace}""/{x=1}x&&/kernelcache.release/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1)" -o work/kcache.raw
+"$dir"/img4 -i work/"$(awk "/""${replace}""/{x=1}x&&/kernelcache.release/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1)" -o work/kcache.raw
 "$dir"/Kernel64Patcher work/kcache.raw work/kcache.patched -a
-python3 -m pyimg4 im4p create -i work/kcache.patched -o work/kernelcache.im4p -f rkrn `if [ "$oscheck" = 'Linux' ]; then echo "--lzss"; fi`
-python3 -m pyimg4 img4 create -p work/kernelcache.im4p -m work/IM4M -o sshramdisk/kernelcache.img4
-python3 -m pyimg4 im4p create -i work/"$(awk "/""${replace}""/{x=1}x&&/DeviceTree[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]all_flash[/]//')" -o work/devicetree.im4p -f rdtr
-python3 -m pyimg4 img4 create -p work/devicetree.im4p -m work/IM4M -o sshramdisk/devicetree.img4
+python3 -m pyimg4 im4p create -i work/kcache.patched -o sshramdisk/kernelcache.im4p -f rkrn `if [ "$oscheck" = 'Linux' ]; then echo "--lzss"; fi`
+python3 -m pyimg4 img4 create -p sshramdisk/kernelcache.im4p -m work/IM4M -o sshramdisk/kernelcache.img4
+rm -rf sshramdisk/kernelcache.im4p
+"$dir"/img4 -i work/"$(awk "/""${replace}""/{x=1}x&&/DeviceTree[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]all_flash[/]//')" -o sshramdisk/devicetree.img4 -M work/IM4M -T rdtr
+
 if [ "$oscheck" = 'Darwin' ]; then
-    python3 -m pyimg4 im4p create -i work/"$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)".trustcache -o work/trustcache.im4p -f rtsc
-    python3 -m pyimg4 img4 create -p work/trustcache.im4p -m work/IM4M -o sshramdisk/trustcache.img4
-    python3 -m pyimg4 im4p extract -i work/"$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" -o work/ramdisk.dmg
+    "$dir"/img4 -i work/"$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)".trustcache -o sshramdisk/trustcache.img4 -M work/IM4M -T rtsc
+    "$dir"/img4 -i work/"$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" -o work/ramdisk.dmg
 else
-    python3 -m pyimg4 im4p create -i work/"$("$dir"/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g')".trustcache -o work/trustcache.im4p -f rtsc
-    python3 -m pyimg4 img4 create -p work/trustcache.im4p -m work/IM4M -o sshramdisk/trustcache.img4
-    python3 -m pyimg4 im4p extract -i work/"$("$dir"/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g')" -o work/ramdisk.dmg
+    "$dir"/img4 -i work/"$("$dir"/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g')".trustcache -o sshramdisk/trustcache.img4 -M work/IM4M -T rtsc
+    "$dir"/img4 -i work/"$("$dir"/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g')" -o work/ramdisk.dmg
 fi
 
 if [ "$oscheck" = 'Darwin' ]; then
@@ -210,7 +207,6 @@ else
 fi
 python3 -m pyimg4 im4p create -i work/ramdisk.dmg -o work/ramdisk.im4p -f rdsk
 python3 -m pyimg4 img4 create -p work/ramdisk.im4p -m work/IM4M -o sshramdisk/ramdisk.img4
-python3 -m pyimg4 im4p create -i other/bootlogo.im4p -o work/bootlogo.im4p -f rlgo
-python3 -m pyimg4 img4 create -p work/bootlogo.im4p -m work/IM4M -o sshramdisk/bootlogo.img4
+"$dir"/img4 -i other/bootlogo.im4p -o sshramdisk/bootlogo.img4 -M work/IM4M -A -T rlgo
 
 rm -rf work
