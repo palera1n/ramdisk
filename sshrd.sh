f08ea6ef58d27b4e354dfb43ec8a583fce275e47
Tom E <guacaplushy@icloud.com>
2022-11-27 13:07:14 -0500

    :trolley: (#13)

--- sshrd.sh
+++ sshrd.sh
@@ -28,10 +28,10 @@ fi
 # git submodule update --init --recursive
 
 if [ ! -e "$dir"/gaster ]; then
-    curl -sLO https://nightly.link/palera1n/gaster/workflows/makefile/main/gaster-"$oscheck".zip
-    unzip gaster-"$oscheck".zip
+    curl -sLO https://nightly.link/palera1n/gaster/workflows/makefile/main/gaster-"$oscheck"-"$arch".zip
+    unzip gaster-"$oscheck"-"$arch".zip
     mv gaster "$dir"/
-    rm -rf gaster gaster-"$oscheck".zip
+    rm -rf gaster gaster-"$oscheck"-"$arch".zip
 fi
 
 chmod +x "$dir"/*
